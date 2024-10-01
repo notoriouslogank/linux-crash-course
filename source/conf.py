f5d0f1920612546827c1d667785dc0a25dce3302
logank <logan.aker1989@gmail.com>
2024-09-30 23:42:57 -0400

    fixing link icon

--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,5 @@
+from sphinxawesome_theme.postprocess import Icons
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
